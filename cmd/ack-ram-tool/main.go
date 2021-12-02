6f69f582c86c9a659f6aaf3833f9a8670d09d0d9
mozillazg <mozillazg101@gmail.com>
2021-12-02 23:09:54 +0800

    Add --profile-file for use a specific credential file

--- cmd/ack-ram-tool/main.go
+++ cmd/ack-ram-tool/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/AliyunContainerService/ack-ram-tool/pkg/ctl"
 	"github.com/AliyunContainerService/ack-ram-tool/pkg/ctl/rrsa"
 	"github.com/AliyunContainerService/ack-ram-tool/pkg/ctl/version"
+	"github.com/aliyun/credentials-go/credentials"
 	"github.com/spf13/cobra"
 )
+
+var defaultProfilePath = filepath.Join("~", ".alibabacloud", "credentials")
+var profilePath = ""
 
 var (
 	rootCmd = &cobra.Command{
@@ -17,6 +23,14 @@ var (
 		Long: `A command line utility for using RAM in ACK.
 
 More info: https://github.com/AliyunContainerService/ack-ram-tool`,
+		PersistentPreRun: func(_ *cobra.Command, _ []string) {
+			path, err := expandPath(profilePath)
+			if err != nil {
+				fmt.Printf("error: parse profile path %s failed: %+v", profilePath, err)
+				os.Exit(1)
+			}
+			_ = os.Setenv(credentials.ENVCredentialFile, path)
+		},
 	}
 )
 
@@ -25,6 +39,18 @@ func init() {
 	version.SetupVersionCmd(rootCmd)
 	rootCmd.PersistentFlags().BoolVarP(&ctl.GlobalOption.AssumeYes, "assume-yes", "y", false,
 		"Automatic yes to prompts; assume \"yes\" as answer to all prompts and run non-interactively")
+	rootCmd.PersistentFlags().StringVar(&profilePath, "profile-file", defaultProfilePath, "Path to credential file")
+}
+
+func expandPath(path string) (string, error) {
+	if len(path) > 0 && path[0] == '~' {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		path = filepath.Join(home, path[1:])
+	}
+	return path, nil
 }
 
 func main() {
