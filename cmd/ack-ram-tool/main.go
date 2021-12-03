f578618367e4f39193ac4ca03424fce9b946e534
Huang Huang <mozillazg101@gmail.com>
2021-12-03 13:04:33 +0800

    Add --profile-file for use a specific credential file and fix env typo (#3)
    * Add --profile-file for use a specific credential file
    
    * improve env key name
    
    * run go mod tidy

--- cmd/ack-ram-tool/main.go
+++ cmd/ack-ram-tool/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/AliyunContainerService/ack-ram-tool/pkg/ctl"
 	"github.com/AliyunContainerService/ack-ram-tool/pkg/ctl/rrsa"
 	"github.com/AliyunContainerService/ack-ram-tool/pkg/ctl/version"
+	"github.com/aliyun/credentials-go/credentials"
 	"github.com/spf13/cobra"
 )
+
+const (
+	envAccessKeyId = "ALIBABA_CLOUD_ACCESS_KEY_ID"
+	envAccessKeySecret = "ALIBABA_CLOUD_ACCESS_KEY_SECRET"
+)
+var defaultProfilePath = filepath.Join("~", ".alibabacloud", "credentials")
+var profilePath = ""
 
 var (
 	rootCmd = &cobra.Command{
@@ -17,6 +27,20 @@ var (
 		Long: `A command line utility for using RAM in ACK.
 
 More info: https://github.com/AliyunContainerService/ack-ram-tool`,
+		PersistentPreRun: func(_ *cobra.Command, _ []string) {
+			if _, ok := os.LookupEnv(envAccessKeyId); ok {
+				_ = os.Setenv(credentials.EnvVarAccessKeyId, os.Getenv(envAccessKeyId))
+			}
+			if _, ok := os.LookupEnv(envAccessKeySecret); ok {
+				_ = os.Setenv(credentials.EnvVarAccessKeySecret, os.Getenv(envAccessKeySecret))
+			}
+			path, err := expandPath(profilePath)
+			if err != nil {
+				fmt.Printf("error: parse profile path %s failed: %+v", profilePath, err)
+				os.Exit(1)
+			}
+			_ = os.Setenv(credentials.ENVCredentialFile, path)
+		},
 	}
 )
 
@@ -25,6 +49,18 @@ func init() {
 	version.SetupVersionCmd(rootCmd)
 	rootCmd.PersistentFlags().BoolVarP(&ctl.GlobalOption.AssumeYes, "assume-yes", "y", false,
 		"Automatic yes to prompts; assume \"yes\" as answer to all prompts and run non-interactively")
+	rootCmd.PersistentFlags().StringVar(&profilePath, "profile-file", defaultProfilePath, "Path to credential file")
+}
+
+func expandPath(path string) (string, error) {
+	if len(path) > 0 && path[0] == '~' {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		path = filepath.Join(home, path[1:])
+	}
+	return path, nil
 }
 
 func main() {
