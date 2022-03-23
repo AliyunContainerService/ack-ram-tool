a9d1fc3985abbce66601154ea82831a360841770
mozillazg <mozillazg101@gmail.com>
2022-03-23 20:23:44 +0800

    add example for oss java sdk

--- examples/rrsa/java-sdk/README.md
+++ examples/rrsa/java-sdk/README.md
@@ -14,5 +14,7 @@ ALIBABA_CLOUD_OIDC_TOKEN_FILE=<oidc_token_file>
 3. Run `Main`:
 
 ```
+call GetCallerIdentity via oidc token success:
+
 {IdentityType=AssumedRoleUser, AccountId=18***, RequestId=69***, PrincipalId=3***:test-rrsa-oidc-token, UserId=null, Arn=acs:ram::18***:assumed-role/test-rrsa-***/test-rrsa-oidc-token, RoleId=3***}
 ```
--- examples/rrsa/java-sdk/src/Main.java
+++ examples/rrsa/java-sdk/src/Main.java
@@ -1,14 +1,83 @@
 
 import com.aliyun.credentials.Client;
-import com.aliyun.tea.*;
-import com.aliyun.sts20150401.*;
 import com.aliyun.sts20150401.models.*;
-import com.aliyun.teaopenapi.*;
-import com.aliyun.teaopenapi.models.*;
-import com.aliyun.credentials.models.Config;
+
+// only for oss sdk
+import com.aliyun.oss.ClientBuilderConfiguration;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.auth.*;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.Bucket;
+
+import java.util.List;
+
+class TestOpenAPISDK {
+
+    public void CallAPIWithRRSAOIDCToken(com.aliyun.credentials.Client cred) throws Exception {
+        // new client config
+        com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config()
+                .setCredential(cred)
+                .setEndpoint("sts.aliyuncs.com");
+
+        // init client
+        com.aliyun.sts20150401.Client client = new com.aliyun.sts20150401.Client(config);
+
+        // call api
+        GetCallerIdentityResponse resp = client.getCallerIdentity();
+        System.out.println("call sts.GetCallerIdentity via oidc token success:\n");
+        System.out.println(resp.getBody().toMap().toString());
+    }
+}
+
+class OSSOIDCTokenCredentialProvider implements CredentialsProvider {
+
+    private volatile com.aliyun.credentials.Client cred;
+
+    public OSSOIDCTokenCredentialProvider(com.aliyun.credentials.Client cred) {
+        this.cred = cred;
+    }
+
+    public void setCredentials(Credentials creds) {
+    }
+
+    @Override
+    public Credentials getCredentials() {
+        String ak = cred.getAccessKeyId();
+        String sk = cred.getAccessKeySecret();
+        String token = cred.getSecurityToken();
+        return new DefaultCredentials(ak, sk, token);
+    }
+}
+
+class TestOSSSDK {
+
+    public void CallAPIWithRRSAOIDCToken(com.aliyun.credentials.Client cred) throws Exception {
+        // new provider
+        OSSOIDCTokenCredentialProvider provider = new OSSOIDCTokenCredentialProvider(cred);
+        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+        // new client config
+        ClientBuilderConfiguration conf = new ClientBuilderConfiguration();
+
+        // init client
+        OSS ossClient = new OSSClientBuilder().build(endpoint, provider, conf);
+
+        // call api
+        List<Bucket> buckets = ossClient.listBuckets();
+        System.out.println("call oss.listBuckets via oidc token success:\n");
+        for (Bucket bucket : buckets) {
+            System.out.println(" - " + bucket.getName());
+        }
+
+        ossClient.shutdown();
+    }
+
+}
+
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
+        // new credential which use rrsa oidc token
         com.aliyun.credentials.models.Config credConf = new com.aliyun.credentials.models.Config();
         credConf.type = "oidc_role_arn";
         credConf.roleArn = System.getenv("ALIBABA_CLOUD_ROLE_ARN");
@@ -18,14 +87,18 @@ public class Main {
         com.aliyun.credentials.Client cred =  new Client(credConf);
 
 
-        com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config()
-                .setCredential(cred)
-                .setEndpoint("sts.aliyuncs.com");
+        // test open api sdk (https://github.com/aliyun/alibabacloud-java-sdk) use rrsa oidc token
+        System.out.println("\n");
+        System.out.println("test open api sdk use rrsa oidc token");
+        TestOpenAPISDK openapiSdk = new TestOpenAPISDK();
+        openapiSdk.CallAPIWithRRSAOIDCToken(cred);
 
-        com.aliyun.sts20150401.Client client = new com.aliyun.sts20150401.Client(config);
-
-
-        GetCallerIdentityResponse resp = client.getCallerIdentity();
-        System.out.println(resp.getBody().toMap().toString());
+        // test oss sdk (https://github.com/aliyun/aliyun-oss-java-sdk) use rrsa oidc token
+        if (System.getenv("TEST-OSS-SDK").equals("true")) {
+            System.out.println("\n");
+            System.out.println("test oss sdk use rrsa oidc token");
+            TestOSSSDK osssdk = new TestOSSSDK();
+            osssdk.CallAPIWithRRSAOIDCToken(cred);
+        }
     }
 }
