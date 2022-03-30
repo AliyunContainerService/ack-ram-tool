e9fdafedd2d00eb9fd4cddaab91a21bcfb1c8bb0
mozillazg <mozillazg101@gmail.com>
2022-03-30 20:09:11 +0800

    add example for oss golang sdk

--- examples/rrsa/go-sdk/main.go
+++ examples/rrsa/go-sdk/main.go
@@ -2,13 +2,86 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
 	sts "github.com/alibabacloud-go/sts-20150401/client"
 	"github.com/alibabacloud-go/tea/tea"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/aliyun/credentials-go/credentials"
 )
+
+func testOpenAPISDK(cred credentials.Credential) {
+	client, err := sts.NewClient(&openapi.Config{
+		Endpoint:   tea.String("sts.aliyuncs.com"),
+		Credential: cred,
+	})
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.GetCallerIdentity()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("call sts.GetCallerIdentity via oidc token success:\n%s\n", resp.String())
+}
+
+func testOSSSDK(cred credentials.Credential) {
+	provider := &ossCredentialsProvider{cred: cred}
+	client, err := oss.New("https://oss-cn-hangzhou.aliyuncs.com", "", "",
+		oss.SetCredentialsProvider(provider))
+	if err != nil {
+		panic(err)
+	}
+	ret, err := client.ListBuckets()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("call oss.listBuckets via oidc token success:")
+	for _, item := range ret.Buckets {
+		fmt.Printf("-%s\n", item.Name)
+	}
+}
+
+type ossCredentials struct {
+	teaCred credentials.Credential
+}
+
+func (cred *ossCredentials) GetAccessKeyID() string {
+	value, err := cred.teaCred.GetAccessKeyId()
+	if err != nil {
+		log.Printf("get access key id failed: %+v", err)
+		return ""
+	}
+	return tea.StringValue(value)
+}
+
+func (cred *ossCredentials) GetAccessKeySecret() string {
+	value, err := cred.teaCred.GetAccessKeySecret()
+	if err != nil {
+		log.Printf("get access key secret failed: %+v", err)
+		return ""
+	}
+	return tea.StringValue(value)
+}
+
+func (cred *ossCredentials) GetSecurityToken() string {
+	value, err := cred.teaCred.GetSecurityToken()
+	if err != nil {
+		log.Printf("get access security token failed: %+v", err)
+		return ""
+	}
+	return tea.StringValue(value)
+}
+
+type ossCredentialsProvider struct {
+	cred credentials.Credential
+}
+
+func (p *ossCredentialsProvider) GetCredentials() oss.Credentials {
+	return &ossCredentials{teaCred: p.cred}
+}
 
 func main() {
 	roleArn := os.Getenv("ALIBABA_CLOUD_ROLE_ARN")
@@ -27,16 +100,13 @@ func main() {
 		panic(err)
 	}
 
-	client, err := sts.NewClient(&openapi.Config{
-		Endpoint:   tea.String("sts.aliyuncs.com"),
-		Credential: oidcCredential,
-	})
-	if err != nil {
-		panic(err)
+	// test open api sdk (https://github.com/aliyun/alibabacloud-go-sdk) use rrsa oidc token
+	fmt.Println("\ntest open api sdk use rrsa oidc token")
+	testOpenAPISDK(oidcCredential)
+
+	// test oss sdk (https://github.com/aliyun/aliyun-oss-go-sdk) use rrsa oidc token
+	if os.Getenv("TEST_OSS_SDK") == "true" {
+		fmt.Println("\ntest oss sdk use rrsa oidc token")
+		testOSSSDK(oidcCredential)
 	}
-	resp, err := client.GetCallerIdentity()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("call GetCallerIdentity via oidc token success:\n%s\n", resp.String())
 }
