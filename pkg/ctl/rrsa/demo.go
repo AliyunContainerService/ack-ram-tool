1ccffb7331707f0d9fc4f1a70f8acc4a5069a85a
mozillazg <mozillazg101@gmail.com>
2023-02-02 16:06:35 +0800

    add --no-loop flag for rrsa demo command

--- pkg/ctl/rrsa/demo.go
+++ pkg/ctl/rrsa/demo.go
@@ -3,9 +3,18 @@ package rrsa
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/AliyunContainerService/ack-ram-tool/pkg/ctl/common"
 	"github.com/spf13/cobra"
 )
+
+type DemoOpts struct {
+	noLoop bool
+}
+
+var demoOpts = DemoOpts{}
 
 var demoCmd = &cobra.Command{
 	Use:   "demo",
@@ -13,14 +22,30 @@ var demoCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		client := common.GetClientOrDie()
-		cs, err := client.ListClusters(context.Background())
-		common.ExitIfError(err)
-		for _, c := range cs {
-			fmt.Printf("cluster id: %s, cluster name: %s\n", c.ClusterId, c.Name)
+		for {
+			log.Println("======= [begin] list ACK clusters with RRSA =======")
+			cs, err := client.ListClusters(context.Background())
+			if demoOpts.noLoop {
+				common.ExitIfError(err)
+			} else if err != nil {
+				log.Println(err)
+			}
+
+			fmt.Println("clusters:")
+			for _, c := range cs {
+				fmt.Printf("cluster id: %s, cluster name: %s\n", c.ClusterId, c.Name)
+			}
+			log.Println("======= [end]   list ACK clusters with RRSA =======")
+			if demoOpts.noLoop {
+				break
+			}
+			log.Println("will try again after 30 seconds")
+			time.Sleep(time.Second * 30)
 		}
 	},
 }
 
 func setupDemoCmd(rootCmd *cobra.Command) {
 	rootCmd.AddCommand(demoCmd)
+	demoCmd.Flags().BoolVar(&demoOpts.noLoop, "no-loop", false, "")
 }
