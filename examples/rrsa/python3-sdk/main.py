e805a7a20e5a9ea81b6781f6af7db3b608066b54
mozillazg <mozillazg101@gmail.com>
2023-02-03 11:46:45 +0800

    use alibabacloud-credentials 0.3.1

--- examples/rrsa/python3-sdk/main.py
+++ examples/rrsa/python3-sdk/main.py
@@ -1,19 +1,15 @@
 # coding: utf-8
-import json
 import os
 
-import oss2
-
-# alibabacloud-credentials>=0.3.0
+# alibabacloud-credentials>=0.3.1
 from alibabacloud_credentials.client import Client as CredClient
 from alibabacloud_credentials.models import Config as CredConfig
 
-from alibabacloud_sts20150401.client import Client as Sts20150401Client
-from alibabacloud_tea_openapi import models as sts_open_api_models
-from alibabacloud_sts20150401 import models as sts_20150401_models
-from alibabacloud_tea_util import models as util_models
-from alibabacloud_tea_util.client import Client as UtilClient
+from alibabacloud_cs20151215.client import Client as CS20151215Client
+from alibabacloud_cs20151215 import models as csmodules
+from alibabacloud_tea_openapi import models as open_api_models
 
+import oss2
 from oss2.credentials import (
     CredentialsProvider as OSSCredentialsProvider,
     Credentials as OSSCredentials
@@ -25,30 +21,45 @@ ENV_OIDC_TOKEN_FILE = "ALIBABA_CLOUD_OIDC_TOKEN_FILE"
 
 
 def test_open_api_sdk(cred):
-    config = sts_open_api_models.Config(
-        # get endpoint from https://www.alibabacloud.com/help/resource-access-management/latest/endpoints
-        endpoint="sts.aliyuncs.com", credential=cred)
-    client = Sts20150401Client(config)
-    resp = client.get_caller_identity()
-    print("call sts.GetCallerIdentity via oidc token success:\n{}\n".format(
-        json.dumps(resp.to_map(), indent=2)))
+    config = open_api_models.Config(credential=cred)
+    config.endpoint = 'cs.cn-hangzhou.aliyuncs.com'
+
+    client = CS20151215Client(config)
+    resp = client.describe_clusters(csmodules.DescribeClustersRequest())
+
+    print("call sts.GetCallerIdentity via oidc token success:\n")
+    for cluster in resp.body:
+        print("cluster id: %s, cluster name: %s\n", cluster.cluster_id, cluster.name)
+    print('\n')
 
 
 def test_oss_sdk(cred):
     endpoint = 'http://oss-cn-hangzhou.aliyuncs.com'
     provider = OSSOidcCredentialProvider(cred)
     auth = oss2.ProviderAuth(provider)
+
     service = oss2.Service(auth=auth, endpoint=endpoint)
     resp = service.list_buckets()
+
     print("call oss.listBuckets via oidc token success:")
     for bucket in resp.buckets:
         print('- {}'.format(bucket.name))
 
 
-def get_oidc_cred(role_arn, oidc_arn, token_file):
-    config = CredConfig(type='oidc_role_arn', role_arn=role_arn,
-                        oidc_provider_arn=oidc_arn, oidc_token_file_path=token_file,
-                        role_session_name='auth-with-rrsa-oidc-token')
+def new_cred():
+    # https://www.alibabacloud.com/help/doc-detail/378661.html
+    cred = CredClient()
+    return cred
+
+
+def new_oidc_cred():
+    # https://www.alibabacloud.com/help/doc-detail/378661.html
+    config = CredConfig(
+        type='oidc_role_arn',
+        role_arn=os.environ[ENV_ROLE_ARN],
+        oidc_provider_arn=os.environ[ENV_OIDC_PROVIDER_ARN],
+        oidc_token_file_path=os.environ[ENV_OIDC_TOKEN_FILE],
+        role_session_name='auth-with-rrsa-oidc-token')
     cred = CredClient(config)
     return cred
 
@@ -66,8 +77,10 @@ class OSSOidcCredentialProvider(OSSCredentialsProvider):
 
 
 def main():
-    cred = get_oidc_cred(os.environ[ENV_ROLE_ARN], os.environ[ENV_OIDC_PROVIDER_ARN],
-                         os.environ[ENV_OIDC_TOKEN_FILE])
+    # 两种方法都可以
+    cred = new_cred()
+    # or
+    # cred = new_oidc_cred()
 
     # test open api sdk (https://github.com/aliyun/alibabacloud-python-sdk) use rrsa oidc token
     print("\ntest open api sdk use rrsa oidc token")
