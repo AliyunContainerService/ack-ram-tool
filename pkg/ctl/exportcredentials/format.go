4cfa600e5bafc061a882ff4233811a4d70b2a7f2
mozillazg <mozillazg101@gmail.com>
2023-03-09 17:32:53 +0800

    add new format `environment-variables` for export-credentials subcommand

--- pkg/ctl/exportcredentials/format.go
+++ pkg/ctl/exportcredentials/format.go
@@ -35,6 +35,40 @@ security_token = {{.StsToken}}
 {{- end }}
 `
 
+var environmentVariablesFormat = `
+for aliyun cli:
+
+export ALIBABACLOUD_ACCESS_KEY_ID={{.AccessKeyId}}
+export ALIBABACLOUD_ACCESS_KEY_SECRET={{.AccessKeySecret}}
+{{- if ne .Mode "AK" }}
+export ALIBABACLOUD_SECURITY_TOKEN={{.StsToken}}
+{{- end }}
+
+for terraform:
+
+export ALICLOUD_ACCESS_KEY={{.AccessKeyId}}
+export ALICLOUD_SECRET_KEY={{.AccessKeySecret}}
+{{- if ne .Mode "AK" }}
+export ALICLOUD_SECURITY_TOKEN={{.StsToken}}
+{{- end }}
+
+for other tools:
+
+export ALIBABA_CLOUD_ACCESS_KEY_ID={{.AccessKeyId}}
+export ALICLOUD_ACCESS_KEY={{.AccessKeyId}}
+export ALIBABACLOUD_ACCESS_KEY_ID={{.AccessKeyId}}
+export ALICLOUD_SECRET_KEY={{.AccessKeyId}}
+export ALIBABA_CLOUD_ACCESS_KEY_SECRET={{.AccessKeySecret}}
+export ALIBABACLOUD_ACCESS_KEY_SECRET={{.AccessKeySecret}}
+{{- if ne .Mode "AK" }}
+export ALIBABA_CLOUD_SECURITY_TOKEN={{.StsToken}}
+export ALICLOUD_SECURITY_TOKEN={{.StsToken}}
+export ALIBABACLOUD_SECURITY_TOKEN={{.StsToken}}
+export ALICLOUD_ACCESS_KEY_STS_TOKEN={{.StsToken}}
+export ALIYUN_LOG_CLI_STS_TOKEN={{.StsToken}}
+{{- end }}
+`
+
 func toAliyunCLIConfig(cred Credentials) AliyunCLIConfig {
 	config := AliyunCLIConfig{
 		Mode:            "AK",
@@ -69,6 +103,14 @@ func toAliyunCLIURIBody(cred Credentials) string {
 func toCredentialFileIni(cred Credentials) string {
 	config := toAliyunCLIConfig(cred)
 	t, _ := template.New("ini").Parse(credentialFileFormat)
+	var buf strings.Builder
+	_ = t.Execute(&buf, config)
+	return buf.String()
+}
+
+func toExportEnvironmentVariables(cred Credentials) string {
+	config := toAliyunCLIConfig(cred)
+	t, _ := template.New("env").Parse(environmentVariablesFormat)
 	var buf strings.Builder
 	_ = t.Execute(&buf, config)
 	return buf.String()
