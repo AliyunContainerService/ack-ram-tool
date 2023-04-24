2a8ebfb61157bb7fc1786513bbd236d6dd933c94
mozillazg <mozillazg101@gmail.com>
2023-04-24 15:42:14 +0800

    website: fix site url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'ack-ram-tool',
+  tagline: 'ack-ram-tool',
+  // favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://aliyuncontainerservice.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ack-ram-tool/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
