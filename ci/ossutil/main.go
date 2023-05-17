926c138f7ec3e7c6932453082a48555b811345ad
mozillazg <mozillazg101@gmail.com>
2023-05-17 14:21:24 +0800

    ci: add new dist publisher

---
+++ ci/ossutil/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"path"
+	"time"
+
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/aliyun/credentials-go/credentials"
+)
+
+type ossCredentials struct {
+	teaCred credentials.Credential
+}
+
+func (cred *ossCredentials) GetAccessKeyID() string {
+	value, err := cred.teaCred.GetAccessKeyId()
+	if err != nil {
+		log.Printf("get access key id failed: %+v", err)
+		return ""
+	}
+	return tea.StringValue(value)
+}
+
+func (cred *ossCredentials) GetAccessKeySecret() string {
+	value, err := cred.teaCred.GetAccessKeySecret()
+	if err != nil {
+		log.Printf("get access key secret failed: %+v", err)
+		return ""
+	}
+	return tea.StringValue(value)
+}
+
+func (cred *ossCredentials) GetSecurityToken() string {
+	value, err := cred.teaCred.GetSecurityToken()
+	if err != nil {
+		log.Printf("get access security token failed: %+v", err)
+		return ""
+	}
+	return tea.StringValue(value)
+}
+
+type ossCredentialsProvider struct {
+	cred credentials.Credential
+}
+
+func (p *ossCredentialsProvider) GetCredentials() oss.Credentials {
+	return &ossCredentials{teaCred: p.cred}
+}
+
+func NewClient(endpoint string) (*oss.Client, error) {
+	cred, err := credentials.NewCredential(nil)
+	if err != nil {
+		return nil, err
+	}
+	provider := &ossCredentialsProvider{cred: cred}
+	client, err := oss.New(endpoint, "", "", oss.SetCredentialsProvider(provider))
+	return client, err
+}
+
+func UploadFile(bucket *oss.Bucket, baseObjectDir, filePath string) (string, error) {
+	objectPath := fmt.Sprintf("%s/%s", baseObjectDir, path.Base(filePath))
+
+	var err error
+	maxN := 5
+	for i := 0; i < maxN; i++ {
+		err = bucket.PutObjectFromFile(objectPath, filePath)
+		if err == nil {
+			return objectPath, nil
+		}
+		log.Printf("%d/%d upload file failed: %s", i+1, maxN, err)
+		if i < maxN {
+			time.Sleep(time.Second * 3 * time.Duration(i+1))
+		}
+	}
+	if err != nil {
+		return "", err
+	}
+	return objectPath, nil
+}
+
+func main() {
+	endpoint := flag.String("endpoint", "", "")
+	bucketName := flag.String("bucket", "", "")
+	objectDir := flag.String("objectdir", "", "")
+	flag.Parse()
+
+	filepathList := flag.Args()
+	if *endpoint == "" || *bucketName == "" || *objectDir == "" || len(filepathList) == 0 {
+		log.Fatalln("missing required arguments")
+	}
+
+	client, err := NewClient(*endpoint)
+	if err != nil {
+		log.Fatalf("init client failed: %s", err)
+	}
+	bucket, err := client.Bucket(*bucketName)
+	if err != nil {
+		log.Fatalf("init bucket client failed: %s", err)
+	}
+
+	for _, ph := range filepathList {
+		_, err = UploadFile(bucket, *objectDir, ph)
+		if err != nil {
+			log.Fatalf("upload %s failed: %s", ph, err)
+		}
+		log.Printf("uploaded %s to oss", ph)
+	}
+}
