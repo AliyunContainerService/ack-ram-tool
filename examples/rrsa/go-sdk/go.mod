fd00710296f079cb2e1e52e9906fe72cb2a7e357
mozillazg <mozillazg101@gmail.com>
2023-05-19 16:05:14 +0800

    example: remove oss related codes

--- examples/rrsa/go-sdk/go.mod
+++ examples/rrsa/go-sdk/go.mod
@@ -7,11 +7,9 @@ require (
 	github.com/alibabacloud-go/darabonba-openapi/v2 v2.0.4
 	github.com/alibabacloud-go/tea v1.1.20
 	github.com/alibabacloud-go/tea-utils v1.3.9 // indirect
-	github.com/aliyun/aliyun-oss-go-sdk v2.2.6+incompatible
 	github.com/aliyun/credentials-go v1.2.6
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
 	gopkg.in/ini.v1 v1.66.6 // indirect
 )
--- examples/rrsa/go-sdk/main.go
+++ examples/rrsa/go-sdk/main.go
@@ -8,7 +8,6 @@ import (
 	cs20151215 "github.com/alibabacloud-go/cs-20151215/v3/client"
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	"github.com/alibabacloud-go/tea/tea"
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	// github.com/aliyun/credentials-go >= v1.2.6
 	"github.com/aliyun/credentials-go/credentials"
 )
@@ -42,37 +41,6 @@ func testOpenAPISDK() {
 	}
 }
 
-func testOSSSDK() {
-	// 两种方法都可以
-	cred := newCredential()
-	// or
-	// cred := newOidcCredential()
-
-	provider := &ossCredentialsProvider{cred: cred}
-	client, err := oss.New("https://oss-cn-hangzhou.aliyuncs.com", "", "",
-		oss.SetCredentialsProvider(provider))
-	if err != nil {
-		panic(err)
-	}
-	ret, err := client.ListBuckets()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("call oss.listBuckets via oidc token success:")
-	for _, item := range ret.Buckets {
-		fmt.Printf("-%s\n", item.Name)
-	}
-}
-
-func newCredential() credentials.Credential {
-	// https://www.alibabacloud.com/help/doc-detail/378661.html
-	cred, err := credentials.NewCredential(nil)
-	if err != nil {
-		panic(err)
-	}
-	return cred
-}
-
 func newOidcCredential() credentials.Credential {
 	// https://www.alibabacloud.com/help/doc-detail/378661.html
 	config := new(credentials.Config).
@@ -89,53 +57,8 @@ func newOidcCredential() credentials.Credential {
 	return oidcCredential
 }
 
-type ossCredentials struct {
-	teaCred credentials.Credential
-}
-
-func (cred *ossCredentials) GetAccessKeyID() string {
-	value, err := cred.teaCred.GetAccessKeyId()
-	if err != nil {
-		log.Printf("get access key id failed: %+v", err)
-		return ""
-	}
-	return tea.StringValue(value)
-}
-
-func (cred *ossCredentials) GetAccessKeySecret() string {
-	value, err := cred.teaCred.GetAccessKeySecret()
-	if err != nil {
-		log.Printf("get access key secret failed: %+v", err)
-		return ""
-	}
-	return tea.StringValue(value)
-}
-
-func (cred *ossCredentials) GetSecurityToken() string {
-	value, err := cred.teaCred.GetSecurityToken()
-	if err != nil {
-		log.Printf("get access security token failed: %+v", err)
-		return ""
-	}
-	return tea.StringValue(value)
-}
-
-type ossCredentialsProvider struct {
-	cred credentials.Credential
-}
-
-func (p *ossCredentialsProvider) GetCredentials() oss.Credentials {
-	return &ossCredentials{teaCred: p.cred}
-}
-
 func main() {
-	// test open api sdk (https://github.com/aliyun/alibabacloud-go-sdk) use rrsa oidc token
+	// test open api sdk (https://github.com/aliyun/alibabacloud-go-sdk) using rrsa oidc token
 	log.Printf("test open api sdk use rrsa oidc token")
 	testOpenAPISDK()
-
-	// test oss sdk (https://github.com/aliyun/aliyun-oss-go-sdk) use rrsa oidc token
-	if os.Getenv("TEST_OSS_SDK") == "true" {
-		log.Printf("test oss sdk use rrsa oidc token")
-		testOSSSDK()
-	}
 }
