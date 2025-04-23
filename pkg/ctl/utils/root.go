d1d3f76b1559844bd1ffa201ae89a24418591c60
mozillazg <mozillazg101@gmail.com>
2025-04-23 20:42:26 +0800

    cli: add utils test-ecs-metadata sub-command

--- pkg/ctl/utils/root.go
+++ pkg/ctl/utils/root.go
@@ -1,1 +1,15 @@
 package utils
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use: "utils",
+}
+
+func init() {
+	setupTestEdCmd(rootCmd)
+}
+
+func SetupUtilsCmd(root *cobra.Command) {
+	root.AddCommand(rootCmd)
+}
