86dcee80e5bff97dddccc714249eabfc9902e3a6
mozillazg <mozillazg101@gmail.com>
2026-01-20 15:20:54 +0800

    auto start oauth flow when credentials was expired

--- pkg/credentials/aliyuncli/aliyuncli.go
+++ pkg/credentials/aliyuncli/aliyuncli.go
@@ -9,6 +9,8 @@ import (
 	"github.com/aliyun/aliyun-cli/v3/cli"
 	"github.com/aliyun/aliyun-cli/v3/config"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -21,9 +23,24 @@ func GetCredentialsProvider(p provider.Profile) (provider.CredentialsProvider, e
 
 	ctx := cli.NewCommandContext(os.Stdout, os.Stderr)
 
-	cp := provider.NewFunctionProvider(func(_ context.Context) (*provider.Credentials, error) {
+	cp := provider.NewFunctionProvider(func(c context.Context) (*provider.Credentials, error) {
 		log.Logger.Debugf("try to get credential from aliyun cli profile %q with fallback mode", p.Name)
 		ret, err := newP.GetCredential(ctx, nil)
+		if err != nil {
+			log.Logger.Warnf("get credential from aliyun cli profile %q failed: %s", p.Name, err)
+			if newP.Mode == config.OAuth {
+				if authErr := runOAuthFlow(c, newP); authErr != nil {
+					log.Logger.Errorf("run OAuth Flow failed: %s", authErr)
+					err = authErr
+				} else {
+					if cred, rerr := getCredentialsViaReload(c, os.Args[0], newP); rerr != nil {
+						//
+					} else {
+						return cred, nil
+					}
+				}
+			}
+		}
 		if err != nil {
 			return nil, fmt.Errorf("can not get credential from aliyun cli profile %q: %w", p.Name, err)
 		}
@@ -41,6 +58,60 @@ func GetCredentialsProvider(p provider.Profile) (provider.CredentialsProvider, e
 	})
 
 	return cp, nil
+}
+
+func getCredentialsViaReload(ctx context.Context,
+	bin string, profile config.Profile) (*provider.Credentials, error) {
+	cmd := []string{
+		bin,
+		"export-credentials",
+		"--profile-name",
+		profile.Name,
+		"-f",
+		"aliyun-cli-config-json",
+	}
+	newP := provider.Profile{
+		Name: "dummy", Mode: provider.External,
+		ProcessCommand: strings.Join(cmd, " ")}
+	cliProfile, err := provider.NewCLIConfigProvider(provider.CLIConfigProviderOptions{
+		ConfigPath:                "",
+		ProfileName:               newP.Name,
+		STSEndpoint:               "",
+		Logger:                    nil,
+		GetProviderForUnknownMode: nil,
+		Config: &provider.Configuration{
+			CurrentProfile: newP.Name,
+			Profiles:       []provider.Profile{newP},
+			MetaPath:       "",
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return cliProfile.Credentials(ctx)
+}
+
+func runOAuthFlow(ctx context.Context, profile config.Profile) error {
+	cmdArgs := buildOAuthCmd(profile)
+	log.Logger.Infof("run OAuth Flow via %s", strings.Join(cmdArgs, " "))
+
+	cmd := exec.CommandContext(ctx, cmdArgs[0], cmdArgs[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func buildOAuthCmd(profile config.Profile) []string {
+	var cmd []string
+	cmd = append(cmd, "aliyun", "configure", "--mode", "OAuth")
+	if profile.OAuthSiteType != "" {
+		cmd = append(cmd, "--oauth-site-type", profile.OAuthSiteType)
+	}
+	if profile.Name != "" && profile.Name != "default" {
+		cmd = append(cmd, "--profile", profile.Name)
+	}
+	return cmd
 }
 
 func newProfile(p provider.Profile) config.Profile {
